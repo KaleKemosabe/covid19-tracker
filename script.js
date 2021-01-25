3ba3785aa438afe8cc1a6fb97a2c60c0208f1999
KaleKemosabe <kale.kemosabe@gmail.com>
2021-01-26 00:15:51 +0200

    image and lines added. animation finished

--- script.js
+++ script.js
@@ -15,14 +15,15 @@ function draw() {
     particles.forEach((p, index) => {
         p.update();
         p.draw();
-    })
+        p.checkParticles(particles.slice(index));
+    });
 }
 
 class Particle {
     constructor() {
         this.pos = createVector(random(width), random(height));
         this.vel = createVector(random(-2, 2), random(-2, 2));
-        this.size = 10;
+        this.size = 5;
     }
 
     update() {
@@ -44,5 +45,16 @@ class Particle {
         if(this.pos.y < 0 || this.pos.y > height) {
             this.vel.y *= -1;
         }
+    }
+
+    checkParticles(particles) {
+        particles.forEach(particle => {
+            const d = dist(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+
+            if(d < 120) {
+                stroke('#ffffff');
+                line(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+            }
+        });
     }
 }

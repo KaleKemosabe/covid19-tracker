acce4202127987875e9773ce92ae3e71ab8cf5d4
KaleKemosabe <kale.kemosabe@gmail.com>
2021-01-25 23:49:39 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const particles = [];
+
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+
+    const particlesLength = Math.floor(window.innerWidth / 10);
+
+    for(let i = 0; i < particlesLength; i++) {
+        particles.push(new Particle());
+    }
+}
+
+function draw() {
+    background(55, 100, 144);
+    particles.forEach((p, index) => {
+        p.update();
+        p.draw();
+    })
+}
+
+class Particle {
+    constructor() {
+        this.pos = createVector(random(width), random(height));
+        this.vel = createVector(random(-2, 2), random(-2, 2));
+        this.size = 10;
+    }
+
+    update() {
+        this.pos.add(this.vel);
+        this.edges();
+    }
+
+    draw() {
+        noStroke();
+        fill('#ffffff'); 
+        circle(this.pos.x, this.pos.y, this.size);
+    }
+
+    edges() {
+        if(this.pos.x < 0 || this.pos.x > width) {
+            this.vel.x *= -1;
+        }
+
+        if(this.pos.y < 0 || this.pos.y > height) {
+            this.vel.y *= -1;
+        }
+    }
+}
